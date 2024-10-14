caa8921e74327ce13a47423ccf5e3969d8a9d549
Kim-Gyuri <gyuring0203@gmail.com>
2024-10-14 16:53:15 +0900

    입력오류 코드 변경 (401- > 400 코드와 함께 로그적용)

--- src/main/java/inf/saveanimals/exception/GlobalExceptionHandler.java
+++ src/main/java/inf/saveanimals/exception/GlobalExceptionHandler.java
@@ -5,7 +5,9 @@ import inf.saveanimals.exception.users.DuplicateEmailException;
 import inf.saveanimals.exception.users.DuplicateNameException;
 import inf.saveanimals.exception.users.UserNotFoundException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -58,5 +60,22 @@ public class GlobalExceptionHandler {
         log.info("이미 좋아요 버튼을 눌렀습니다.", exception);
         return LIKE_ALREADY_EXISTS_EXCEPTION;
     }
+
+    // 입력 오류
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<String> handleHttpMessageNotReadableException(HttpMessageNotReadableException ex) {
+        if (ex.getMessage().contains("BreedGroup")) {
+            String errorMessage = "동물 입력값이 잘못되었습니다. 허용 가능한 값은 [ETC, CAT, DOG]입니다.";
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorMessage);
+        }
+
+        if (ex.getMessage().contains("Breed")) {
+            String errorMessage = "품종 입력값이 잘못되었습니다.";
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorMessage);
+        }
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("입력 값이 올바르지 않습니다.");
+    }
+
 
 }
